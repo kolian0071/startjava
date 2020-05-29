e2f7577ca9b08ed1b9c90c3e7d70c103d7dff302
Nikolay Frolkov <kolian0071@gmail.com>
2020-05-29 22:14:12 +0300

    add MyFirstApp and Variable

---
+++ Lesson 1/Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+	public static void main (String[] args) {
+	byte a = 1;
+	short b = 2;
+	int c = 3;
+	long d = 4;
+	float e = 5.0f;
+	double f = 6.00;
+	char g = '#';
+	boolean h = true;
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+	System.out.println(e);
+	System.out.println(f);
+	System.out.println(g);
+	System.out.println(h);
+	}
+}
