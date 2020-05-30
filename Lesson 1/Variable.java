8a0c1f145a8fbf70c15ccbc5083663b2bbbf80ea
Nikolay Frolkov <kolian0071@gmail.com>
2020-05-30 10:07:54 +0300

    переименовал переменные в методе main, показал вложенность кода внутри метода main, разделил блок объявления переменных и их вывода, убрал пробел после main

--- Lesson 1/Variable.java
+++ Lesson 1/Variable.java
@@ -1,20 +1,21 @@
 public class Variable {
-	public static void main (String[] args) {
-	byte a = 1;
-	short b = 2;
-	int c = 3;
-	long d = 4;
-	float e = 5.0f;
-	double f = 6.00;
-	char g = '#';
-	boolean h = true;
-	System.out.println(a);
-	System.out.println(b);
-	System.out.println(c);
-	System.out.println(d);
-	System.out.println(e);
-	System.out.println(f);
-	System.out.println(g);
-	System.out.println(h);
+	public static void main(String[] args) {
+		byte byteNum = 1;
+		short shortNum = 2;
+		int intNum = 3;
+		long longNum = 4;
+		float floatNum = 5.0f;
+		double doubleNum = 6.00;
+		char sym = '#';
+		boolean bool = true;
+
+		System.out.println(byteNum);
+		System.out.println(shortNum);
+		System.out.println(intNum);
+		System.out.println(longNum);
+		System.out.println(floatNum);
+		System.out.println(doubleNum);
+		System.out.println(sym);
+		System.out.println(bool);
 	}
 }
