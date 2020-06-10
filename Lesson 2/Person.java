2407dab688610744fb670fbe36afcd734f094e79
Nikolay Frolkov <kolian0071@gmail.com>
2020-06-10 23:55:05 +0300

    ДЗ второго урока, создал класс Person (без метода main)

---
+++ Lesson 2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+	
+	String name = "Nik";
+	boolean sex = true;
+	int age = 21;
+	int height = 182;
+	int weight = 83;
+
+	void moove() {
+
+	}
+
+	void sit() {
+
+	}
+
+	void run() {
+
+	}
+
+	String speak() {
+		return "Текст сообщения";
+	}
+
+	void studyJava() {
+		
+	}
+}
